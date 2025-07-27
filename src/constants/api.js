9aff7ea0a29ba9ba5b6c06978da185c6ba17f8e1
Nguyễn Cao Minh Tuấn <minhtuantn2406@gmail.com>
2025-07-27 18:13:52 +0700

    feat: update to singapore api

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,2 @@
 // export const API_BASE = "http://localhost:3001";
-export const API_BASE = "https://trivia-scoring-backend-h4l8.onrender.com";
+export const API_BASE = "https://trivia-scoring-backend-rv6k.onrender.com";
